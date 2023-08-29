f3cfa51fedb2cdf616ec5fdc9418784b55b9b0f1
Acesif <36653429+Acesif@users.noreply.github.com>
2023-08-29 12:49:56 +0600

    Generate Parenthesis

---
+++ Generate Parenthesis/gen_par.py
@@ -0,0 +1,20 @@
+def gen_par(n):
+    stack = []
+    res = []
+
+    def backtrack(openN,closeN):
+        if openN==closeN==n:
+            res.append("".join(stack))
+            return
+        if openN<n:
+            stack.append("(")
+            backtrack(openN+1,closeN)
+            stack.pop()
+        if closeN<openN:
+            stack.append(")")
+            backtrack(openN,closeN+1)
+            stack.pop()
+    backtrack(0,0)
+    return res
+
+print(gen_par(3))
